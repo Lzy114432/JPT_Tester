90e4c58a24a3be527d5cd81cbf758ac48f1bb23a
hanHHHyU <1046365096@qq.com>
2025-08-26 21:01:57 +0800

    添加菜单

--- MarkingMachineFeeder/Viewmodel/MainWindowViewModel.cs
+++ MarkingMachineFeeder/Viewmodel/MainWindowViewModel.cs
@@ -20,10 +20,16 @@ namespace MarkingMachineFeeder.Viewmodel
         private string _languageMenuHeader = "Language";
         private string _testLogButtonText = "Test Log";
         private string _currentUserText = "未登录";
-        private string _logoutButtonText = "注销";
-        private string _switchUserButtonText = "切换用户";
-        private string _systemManagementMenuHeader = "系统管理";
-        private string _permissionConfigMenuHeader = "权限配置";
+        private string _currentUser = "Guest";
+        private string _currentUserLabel = "";
+        private string _loginButtonText = "";
+        private string _logoutButtonText = "";
+        private string _switchUserButtonText = "";
+        private string _systemManagementMenuHeader = "";
+        private string _permissionConfigMenuHeader = "";
+        private string _settingsMenuHeader = "";
+        private string _systemMenuHeader = "";
+        private string _systemSettingsMenuHeader = "";
         private bool _canControlCamera = false;
         private bool _canControlUPS = false;
         private bool _canViewSettings = false;
@@ -47,10 +53,28 @@ namespace MarkingMachineFeeder.Viewmodel
             set { SetProperty(ref _testLogButtonText, value); }
         }
 
+        public string CurrentUser
+        {
+            get { return _currentUser; }
+            set { SetProperty(ref _currentUser, value); }
+        }
+
+        public string LoginButtonText
+        {
+            get { return _loginButtonText; }
+            set { SetProperty(ref _loginButtonText, value); }
+        }
+
         public string CurrentUserText
         {
             get { return _currentUserText; }
             set { SetProperty(ref _currentUserText, value); }
+        }
+
+        public string CurrentUserLabel
+        {
+            get { return _currentUserLabel; }
+            set { SetProperty(ref _currentUserLabel, value); }
         }
 
         public string LogoutButtonText
@@ -89,12 +113,29 @@ namespace MarkingMachineFeeder.Viewmodel
             set { SetProperty(ref _permissionConfigMenuHeader, value); }
         }
 
+        public string SettingsMenuHeader
+        {
+            get { return _settingsMenuHeader; }
+            set { SetProperty(ref _settingsMenuHeader, value); }
+        }
+
+        public string SystemMenuHeader
+        {
+            get { return _systemMenuHeader; }
+            set { SetProperty(ref _systemMenuHeader, value); }
+        }
+
+        public string SystemSettingsMenuHeader
+        {
+            get { return _systemSettingsMenuHeader; }
+            set { SetProperty(ref _systemSettingsMenuHeader, value); }
+        }
+
         public bool CanViewSettings
         {
             get { return _canViewSettings; }
             set { SetProperty(ref _canViewSettings, value); }
         }
-
         public bool CanSwitchLanguage
         {
             get { return _canSwitchLanguage; }
@@ -103,8 +144,11 @@ namespace MarkingMachineFeeder.Viewmodel
         public DelegateCommand<string> SwitchLanguageCommand { get; }
         public DelegateCommand TestLogCommand { get; }
         public DelegateCommand LogoutCommand { get; }
+        public DelegateCommand LoginCommand { get; }
         public DelegateCommand SwitchUserCommand { get; }
         public DelegateCommand OpenPermissionConfigCommand { get; }
+        public DelegateCommand OpenSettingsCommand { get; }
+        public DelegateCommand OpenSystemCommand { get; }
 
         public MainWindowViewModel()
         {
@@ -122,8 +166,11 @@ namespace MarkingMachineFeeder.Viewmodel
             SwitchLanguageCommand = new DelegateCommand<string>(ExecuteSwitchLanguage);
             TestLogCommand = new DelegateCommand(ExecuteTestLog);
             LogoutCommand = new DelegateCommand(ExecuteLogout);
+            LoginCommand = new DelegateCommand(ExecuteLogin);
             SwitchUserCommand = new DelegateCommand(ExecuteSwitchUser);
             OpenPermissionConfigCommand = new DelegateCommand(ExecuteOpenPermissionConfig, CanOpenPermissionConfig);
+            OpenSettingsCommand = new DelegateCommand(ExecuteOpenSettings, CanOpenSettings);
+            OpenSystemCommand = new DelegateCommand(ExecuteOpenSystem, CanOpenSystem);
 
             UpdateUITexts();
             UpdateUserInfo();
@@ -177,6 +224,16 @@ namespace MarkingMachineFeeder.Viewmodel
             }
         }
 
+        private void ExecuteLogin()
+        {
+            // 与SwitchUser功能相同，打开登录窗口
+            var loginWindow = new MarkingMachineFeeder.Windows.LoginWindow();
+            if (loginWindow.ShowDialog() == true)
+            {
+                // 用户登录成功，界面会自动更新
+            }
+        }
+
         private void ExecuteLogout()
         {
             _securityManager.Logout();
@@ -191,6 +248,34 @@ namespace MarkingMachineFeeder.Viewmodel
                 UpdatePermissions();
                 _uiLogger.Info(() => Ewan.Resources.LogMessages.SystemInitialized);
             }
+        }
+
+        private void ExecuteOpenSettings()
+        {
+            // 打开设置窗口的逻辑
+            _uiLogger.Info(() => Ewan.Resources.LogMessages.SystemInitialized);
+            System.Windows.MessageBox.Show("设置功能将在未来版本中实现", "提示", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Information);
+        }
+
+        private bool CanOpenSettings()
+        {
+            // 检查用户是否有权限访问设置
+            return _securityManager.IsAuthenticated && 
+                   _securityManager.CurrentUser?.Roles.Any(r => r.Name == "Administrator" || r.Name == "Engineer") == true;
+        }
+
+        private void ExecuteOpenSystem()
+        {
+            // 打开系统窗口的逻辑
+            _uiLogger.Info(() => Ewan.Resources.LogMessages.SystemInitialized);
+            System.Windows.MessageBox.Show("系统管理功能将在未来版本中实现", "提示", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Information);
+        }
+
+        private bool CanOpenSystem()
+        {
+            // 检查用户是否有权限访问系统管理
+            return _securityManager.IsAuthenticated && 
+                   _securityManager.CurrentUser?.Roles.Any(r => r.Name == "Administrator") == true;
         }
 
         private bool CanOpenPermissionConfig()
@@ -222,14 +307,18 @@ namespace MarkingMachineFeeder.Viewmodel
                 var localizedDisplayName = GetLocalizedUserDisplayName(user.Username);
                 var localizedRoleNames = string.Join(", ", user.Roles.Select(r => GetLocalizedRoleDisplayName(r.Name)));
                 
+                CurrentUser = localizedDisplayName;
                 CurrentUserText = string.Format(Ewan.Resources.UIStrings.CurrentUser, $"{localizedDisplayName} [{localizedRoleNames}]");
             }
             else
             {
+                // 使用硬编码值，因为UIStrings中没有对应的资源
+                CurrentUser = "游客";
                 CurrentUserText = Ewan.Resources.UIStrings.NoUserLoggedIn;
             }
             
-            // 强制触发CurrentUserText的PropertyChanged事件
+            // 强制触发PropertyChanged事件
+            RaisePropertyChanged(nameof(CurrentUser));
             RaisePropertyChanged(nameof(CurrentUserText));
         }
 
@@ -263,6 +352,37 @@ namespace MarkingMachineFeeder.Viewmodel
             }
         }
 
+        private void UpdateUITexts()
+        {
+            Title = Ewan.Resources.UIStrings.MainWindowTitle;
+            LanguageMenuHeader = Ewan.Resources.UIStrings.LanguageMenuHeader;
+            TestLogButtonText = Ewan.Resources.UIStrings.TestLogButton;
+            // 使用资源字符串替代硬编码值
+            LoginButtonText = Ewan.Resources.UIStrings.LoginButtonText;
+            LogoutButtonText = Ewan.Resources.UIStrings.LogoutButton;
+            SwitchUserButtonText = Ewan.Resources.UIStrings.SwitchUserButton;
+            SystemManagementMenuHeader = Ewan.Resources.UIStrings.SystemManagementMenu;
+            PermissionConfigMenuHeader = Ewan.Resources.UIStrings.PermissionConfigMenu;
+            // 使用ResourceManager直接获取资源字符串，如果资源不存在则使用默认值
+            SettingsMenuHeader = Ewan.Resources.UIStrings.ResourceManager.GetString("SettingsMenu", Ewan.Resources.UIStrings.Culture) ?? "设置";
+            SystemMenuHeader = Ewan.Resources.UIStrings.ResourceManager.GetString("SystemMenu", Ewan.Resources.UIStrings.Culture) ?? "系统";
+            SystemSettingsMenuHeader = Ewan.Resources.UIStrings.ResourceManager.GetString("SystemSettingsMenu", Ewan.Resources.UIStrings.Culture) ?? "系统设置";
+            CurrentUserLabel = Ewan.Resources.UIStrings.ResourceManager.GetString("CurrentUserLabel", Ewan.Resources.UIStrings.Culture) ?? "当前用户：";
+            
+            // 强制触发所有相关属性的PropertyChanged事件
+            RaisePropertyChanged(nameof(Title));
+            RaisePropertyChanged(nameof(LanguageMenuHeader));
+            RaisePropertyChanged(nameof(TestLogButtonText));
+            RaisePropertyChanged(nameof(LoginButtonText));
+            RaisePropertyChanged(nameof(LogoutButtonText));
+            RaisePropertyChanged(nameof(SwitchUserButtonText));
+            RaisePropertyChanged(nameof(SystemManagementMenuHeader));
+            RaisePropertyChanged(nameof(PermissionConfigMenuHeader));
+            RaisePropertyChanged(nameof(SettingsMenuHeader));
+            RaisePropertyChanged(nameof(SystemMenuHeader));
+            RaisePropertyChanged(nameof(SystemSettingsMenuHeader));
+            RaisePropertyChanged(nameof(CurrentUserLabel));
+        }
         private void UpdatePermissions()
         {
             CanControlCamera = _securityManager.HasPermission(PermissionResources.Camera, PermissionActions.Control);
@@ -280,26 +400,6 @@ namespace MarkingMachineFeeder.Viewmodel
             {
                 CanSwitchLanguage = false; // 未登录用户不能切换语言
             }
-        }
-
-        private void UpdateUITexts()
-        {
-            Title = Ewan.Resources.UIStrings.MainWindowTitle;
-            LanguageMenuHeader = Ewan.Resources.UIStrings.LanguageMenuHeader;
-            TestLogButtonText = Ewan.Resources.UIStrings.TestLogButton;
-            LogoutButtonText = Ewan.Resources.UIStrings.LogoutButton;
-            SwitchUserButtonText = Ewan.Resources.UIStrings.SwitchUserButton;
-            SystemManagementMenuHeader = Ewan.Resources.UIStrings.SystemManagementMenu;
-            PermissionConfigMenuHeader = Ewan.Resources.UIStrings.PermissionConfigMenu;
-            
-            // 强制触发所有相关属性的PropertyChanged事件
-            RaisePropertyChanged(nameof(Title));
-            RaisePropertyChanged(nameof(LanguageMenuHeader));
-            RaisePropertyChanged(nameof(TestLogButtonText));
-            RaisePropertyChanged(nameof(LogoutButtonText));
-            RaisePropertyChanged(nameof(SwitchUserButtonText));
-            RaisePropertyChanged(nameof(SystemManagementMenuHeader));
-            RaisePropertyChanged(nameof(PermissionConfigMenuHeader));
         }
     }
 }
